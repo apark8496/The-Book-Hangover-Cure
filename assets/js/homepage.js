13217d61cf6ee7ff8bbd2289ca72780482b1a00b
Ashley <adunderwood96@gmail.com>
2021-12-19 20:24:19 -0500

    fixed up JW drink api location

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -16,6 +16,7 @@ function handleSearchClick() {
   var search = document.getElementById("search").value;
   if (document.getElementById("search").value !== "") {
 
+    drinkClick("vodka");
     bookSearched(search);
     recommendedBook(search);
 
@@ -26,6 +27,48 @@ function handleSearchClick() {
   }
 }
 
+
+// recommended drink with your searched book
+var drinkClick = function (drinkChoice) {
+  getDrinkID(drinkChoice)
+}
+
+var getDrinkID = function (liquor) {
+  $('#drink').empty();
+
+  var apiUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + liquor
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          var randomNumber = Math.floor(Math.random() * data.drinks.length)
+          var drinkName = data.drinks[randomNumber].strDrink
+          console.log(data);
+          console.log(drinkName);
+
+          //Image creation
+          var drinkPic = document.createElement("Img");
+          drinkPic.className = ("cardImageDrink");
+          drinkPic.setAttribute('src', data.drinks[randomNumber].strDrinkThumb);
+          console.log(data.drinks[randomNumber].strDrinkThumb);
+
+          var drinkEl = document.getElementById("drink");
+          drinkEl.appendChild(drinkPic);
+
+          //drinkTitle
+          var drinkTitle = document.createElement("h3");
+          drinkTitle.className = "card-text is-size-3";
+
+          drinkTitle.textContent = data.drinks[randomNumber].strDrink;
+          console.log(data.drinks[randomNumber].strDrink);
+
+          drinkEl.appendChild(drinkTitle)
+
+        })
+      }
+    })
+}
 // Current Book Search
 function bookSearched(search) {
   var getBook = document.getElementById("book-div");
@@ -41,7 +84,7 @@ function bookSearched(search) {
 
       // Card Container
       var bookEl = document.getElementById("book");
-      bookEl.className = ""; 
+      bookEl.className = "";
 
       // Book Title
       var bookTitle = document.createElement("h3");
@@ -80,20 +123,15 @@ function bookSearched(search) {
       description.textContent = data.items[0].volumeInfo.description;
       // console.log(data.items[0].volumeInfo.description);
 
-
-      // Add in API fetch for Drink REC
-      
       card.appendChild(bookTitle);
       card.appendChild(bookAuthor);
       card.appendChild(bookCover);
       card.appendChild(rating);
       card.appendChild(tags);
       card.appendChild(description);
-      // append drink to card element
       cardContainer.appendChild(card);
       bookEl.appendChild(cardContainer);
     });
-  // console.log(api);
 }
 
 // Function for recommended books
@@ -112,56 +150,54 @@ function recommendedBook(search) {
       // for loop to show max 10 books
       for (i = 1; i <= 10; i++) {
 
-      // Card Container
-      var recBookEl = document.getElementById("random");
-      recBookEl.className = "";
+        // Card Container
+        var recBookEl = document.getElementById("random");
+        recBookEl.className = "";
 
-      // Book Title
-      var recBookTitle = document.createElement("h3");
-      recBookTitle.className = "card-header-title is-size-3";
-      recBookTitle.textContent = data.items[i].volumeInfo.title;
-      // console.log(data.items[0].volumeInfo.title);
+        // Book Title
+        var recBookTitle = document.createElement("h3");
+        recBookTitle.className = "card-header-title is-size-3";
+        recBookTitle.textContent = data.items[i].volumeInfo.title;
+        // console.log(data.items[0].volumeInfo.title);
 
-      var recCardContainer = document.createElement("div");
-      recCardContainer.className = "card";
+        var recCardContainer = document.createElement("div");
+        recCardContainer.className = "card";
 
-      var recCard = document.createElement("div");
-      recCard.className = "card-content";
+        var recCard = document.createElement("div");
+        recCard.className = "card-content";
 
-      // Book Author
-      var recBookAuthor = document.createElement("h4");
-      recBookAuthor.className = "card-header-title is-size-4";
-      recBookAuthor.textContent = data.items[i].volumeInfo.authors;
+        // Book Author
+        var recBookAuthor = document.createElement("h4");
+        recBookAuthor.className = "card-header-title is-size-4";
+        recBookAuthor.textContent = data.items[i].volumeInfo.authors;
 
-      // Book Cover
-      var recBookCover = document.createElement("img");
-      recBookCover.className = "card-image m-3 p-2";
-      recBookCover.setAttribute('src', data.items[i].volumeInfo.imageLinks.thumbnail);
-      // console.log(data.items[0].volumeInfo.imageLinks.thumbnail);
+        // Book Cover
+        var recBookCover = document.createElement("img");
+        recBookCover.className = "card-image m-3 p-2";
+        recBookCover.setAttribute('src', data.items[i].volumeInfo.imageLinks.thumbnail);
+        // console.log(data.items[0].volumeInfo.imageLinks.thumbnail);
 
-      var recRating = document.createElement("h4");
-      recRating.className = "card-header-title is-size-6";
-      recRating.textContent = "Average Rating: " + data.items[i].volumeInfo.averageRating;
-  
-      // Book Synopsis
-      var recDescription = document.createElement("p");
-      recDescription.className = "card-text is-size-6";
-      recDescription.textContent = data.items[i].volumeInfo.description;
-      // console.log(data.items[0].volumeInfo.description);
+        var recRating = document.createElement("h4");
+        recRating.className = "card-header-title is-size-6";
+        recRating.textContent = "Average Rating: " + data.items[i].volumeInfo.averageRating;
 
-      recCard.appendChild(recBookTitle);
-      recCard.appendChild(recBookAuthor);
-      recCard.appendChild(recBookCover);
-      recCard.appendChild(recRating);
-      recCard.appendChild(recDescription);
-      recCardContainer.appendChild(recCard);
-      recBookEl.appendChild(recCardContainer);
+        // Book Synopsis
+        var recDescription = document.createElement("p");
+        recDescription.className = "card-text is-size-6";
+        recDescription.textContent = data.items[i].volumeInfo.description;
+        // console.log(data.items[0].volumeInfo.description);
+
+        recCard.appendChild(recBookTitle);
+        recCard.appendChild(recBookAuthor);
+        recCard.appendChild(recBookCover);
+        recCard.appendChild(recRating);
+        recCard.appendChild(recDescription);
+        recCardContainer.appendChild(recCard);
+        recBookEl.appendChild(recCardContainer);
       }
     });
 }
 
-// make buttons to add books to tbr
-// save tbr to local storage --> page for tbr js
 
 function saveSearch(search) {
   if (!searchHistory.includes(search)) {
@@ -184,6 +220,7 @@ function searchList() {
     historyItem.textContent = search;
 
     historyItem.addEventListener("click", function (event) {
+      getDrinkID(event.target.textContent);
       bookSearched(event.target.textContent);
       recommendedBook(event.target.textContent);
     });
@@ -192,7 +229,7 @@ function searchList() {
 }
 
 // Make a button to clear Search History
-$(".clearBtn").on("click", function(event) {
+$(".clearBtn").on("click", function (event) {
   event.preventDefault();
   localStorage.clear();
   location.reload();
